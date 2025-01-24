765e1aae27653dbf2bc434eede7f4003a2763907
Rhythm Jain <124446653+rhythmjain1@users.noreply.github.com>
2025-01-25 00:18:48 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-// export const USER_API_END_POINT="";
-// export const JOB_API_END_POINT="";
-// export const APPLICATION_API_END_POINT="";
-// export const COMPANY_API_END_POINT="";
+export const USER_API_END_POINT="";
+export const JOB_API_END_POINT="";
+export const APPLICATION_API_END_POINT="";
+export const COMPANY_API_END_POINT="";
