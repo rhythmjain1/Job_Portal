0b6bcb7d5c38795742bc842353caf8c795819cf4
Rhythm Jain <124446653+rhythmjain1@users.noreply.github.com>
2025-01-25 23:39:09 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,8 +16,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-const cors = require('cors');
-
 
 const corsOptions = {
     origin:'https://job-portal-orpin-six.vercel.app',
