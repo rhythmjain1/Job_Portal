88c902ad4720f9910d77f7f082136cbac0b7cb59
Rhythm Jain <124446653+rhythmjain1@users.noreply.github.com>
2025-08-12 22:52:14 +0530

    Update CompanySetup.jsx

--- frontend/src/components/admin/CompanySetup.jsx
+++ frontend/src/components/admin/CompanySetup.jsx
@@ -132,14 +132,13 @@ const CompanySetup = () => {
                             />
                         </div>
                     </div>
-                    {
-                        loading ? <Button className="w-full my-4"> <Loader2 className='mr-2 h-4 w-4 animate-spin' /> Please wait </Button> : <Button type="submit" className="w-full my-4">Update</Button>
-                    }
+                    <Button type="submit" className="w-full my-4">Update</Button>
                 </form>
             </div>
 
         </div>
     )
 }
+
 
 export default CompanySetup
