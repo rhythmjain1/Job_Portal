c4a4341996b9f152733a1ee3c30ccfea05179236
Rhythm Jain <124446653+rhythmjain1@users.noreply.github.com>
2025-08-12 23:31:29 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,12 +11,13 @@ import { PersistGate } from 'redux-persist/integration/react'
 const persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
         <Toaster />
       </PersistGate>
     </Provider>
-  </React.StrictMode>,
+  </>,
 )
+
